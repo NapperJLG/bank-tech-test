fbe5cbc4acf603257526f74e322750a8d909be4e
Jake Napper <napperjlg@gmail.com>
2019-06-04 19:22:06 +0100

    new debit transaction details can be printed in statement form

--- spec/debit-spec.js
+++ spec/debit-spec.js
@@ -1,8 +1,20 @@
 describe('debit', function(){
 
-  it('stores the amount being debited from an account', function(){
-    debit = new Debit(200.00);
-    expect(debit.amount).toEqual(-200.00);
+  beforeEach(function(){
+    debit = new Debit(200.50);
+    today = new Date();
+  });
+
+  it('stores the amount being withdrawn from the account', function(){
+    expect(debit.amount).toEqual('-200.50');
+  });
+
+  it('stores the date the debit was made', function(){
+    expect(debit.debitDate).toEqual(new Date().toLocaleDateString("en-US"))
+  });
+
+  it('stores the details of a transaction in statement format', function(){
+    expect(debit.transactionDetails).toEqual(['6/4/2019 || -200.50'])
   });
 
 });
--- src/debit.js
+++ src/debit.js
@@ -1,3 +1,5 @@
 var Debit = function(amount){
-  this.amount = -(amount)
+  this.amount = (-amount).toFixed(2)
+  this.debitDate = new Date().toLocaleDateString("en-US");
+  this.transactionDetails = [this.debitDate + ' || ' + this.amount]
 };
